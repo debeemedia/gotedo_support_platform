5f556a70db13b86416636a70447f0779c0351532
Deborah Okeke <okekedeborah@gmail.com>
2023-12-13 09:56:09 +0100

    feat: project setup, configs and database migration

---
+++ database/migrations/1702455865059_users.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string("email_address").unique().notNullable();
+      table.string("full_name").notNullable();
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1702456159372_support_requests.ts
@@ -0,0 +1,29 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'support_requests'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string("email_address").unique().notNullable();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("title").notNullable();
+      table.text("message").notNullable();
+      table.string("file_path").nullable();
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
