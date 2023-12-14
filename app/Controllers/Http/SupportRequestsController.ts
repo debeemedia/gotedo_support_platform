7ccdeb8e32d9375883fc0b7af98b4e9bee609e2f
Deborah Okeke <okekedeborah@gmail.com>
2023-12-14 10:44:00 +0100

    feat: submit request; file upload not handled yet

---
+++ app/Controllers/Http/SupportRequestsController.ts
@@ -0,0 +1,57 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import SupportRequest from 'App/Models/SupportRequest';
+import User from 'App/Models/User';
+
+export default class SupportRequestsController {
+    public async submitRequest ({request, response}: HttpContextContract) {
+        try {
+            const validationSchema = schema.create({
+                email_address: schema.string([
+                    rules.minLength(10),
+                    rules.trim(),
+                    rules.unique({ table: 'support_requests', column: 'email_address' })
+                ]),
+                first_name: schema.string(),
+                last_name: schema.string(),
+                title: schema.string(),
+                message: schema.string(),
+                file_path: schema.string.optional()
+            })
+
+            const newRequestDetails = await request.validate({schema: validationSchema})
+
+            let user = await User.findBy('email_address', newRequestDetails.email_address)
+            if (!user) {
+                user = await User.create({
+                    email_address: newRequestDetails.email_address,
+                    full_name: `${newRequestDetails.first_name} ${newRequestDetails.last_name}`
+                })
+            }
+
+            const newRequest = new SupportRequest()
+            newRequest.email_address = newRequestDetails.email_address
+            newRequest.first_name = newRequestDetails.first_name
+            newRequest.last_name = newRequestDetails.last_name
+            newRequest.title = newRequestDetails.title
+            newRequest.message = newRequestDetails.message
+            newRequest.file_path = newRequestDetails.file_path ?? null
+            
+            // await newRequest.save()
+
+            await user.related('supportRequests').save(newRequest)
+
+            response.status(201).json({
+                message: 'Request submitted successfully'
+            })
+
+        } catch (error) {
+            response.status(500).json({
+                message: 'Failed to submit request',
+                error: error.message
+            })
+            console.log(error.message);
+            
+        }
+    }
+}
